9636b259ec1a82c09c1018a1b0a68476e1da902f
rodrigo4ka <rori.4k@gmail.com>
2023-09-27 20:12:01 -0400

    variables

---
+++ archivo.py
@@ -0,0 +1,42 @@
+#str
+nombre1 = "Armando "
+print(nombre1)
+
+#str
+nombre2 = "Gregorio "
+print(nombre2)
+
+#int
+edad_años1 = 70
+print(edad_años1)
+
+#int
+edad_años2 = 40
+print(edad_años2)
+
+#float
+meses_desde_su_cumpleaños1 = 0.4
+print(meses_desde_su_cumpleaños1)
+#los meses estan representados con decimales
+
+#float
+meses_desde_su_cumpleaños2 = 0.8
+print(meses_desde_su_cumpleaños2)
+#los meses estan representados con decimales
+
+#boolean
+tercera_edad_true = True
+print(tercera_edad_true)
+
+#boolean
+tercera_edad_false = False
+print(tercera_edad_false)
+
+#resultados/print
+print(nombre1 + "tiene" , edad_años1 + meses_desde_su_cumpleaños1 , "años," , "tercera edad" ,  tercera_edad_true)
+
+print(nombre2 + "tiene" , edad_años2 + meses_desde_su_cumpleaños2 , "años," , "tercera edad" ,  tercera_edad_false)
+
+print("-----------------------------------------------------------------------------------------------------------------------")
+
+print("cabe destacar q los meses estan representados como decimales y los años como numeros enteros en el resultado de la edad")
